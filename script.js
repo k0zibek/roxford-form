3cc0475275f5a44030303a48208aa252733359fd
Kazi <uidboy01@gmail.com>
2024-10-09 01:31:46 +0500

    media queries

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const form = document.getElementById('my-form');
 const errorMessage = document.getElementById('error-message');
 
-form.addEventListener('submit', function (event) {
+form.addEventListener('submit', async function (event) {
 	event.preventDefault();
 	const name = document.getElementById('name').value.trim();
 	const phone = document.getElementById('phone').value.trim();
@@ -18,16 +18,20 @@ form.addEventListener('submit', function (event) {
 	formData.append('entry.1537435478', name);
 	formData.append('entry.1907448561', phone);
 	formData.append('entry.1034865502', lesson);
-
-	fetch('https://docs.google.com/forms/d/e/1FAIpQLSeZG0ZFC29GESiazhx4reSPvPo1OV5IVrz1AIDSfzp-myHZWg/formResponse', {
-		method: 'POST',
-		body: formData,
-		mode: 'no-cors',
-	})
-		.then((response) => {
-			console.log('Form submitted successfully');
-		})
-		.catch((error) => {
-			console.error('Error submitting form:', error);
-		});
+	try {
+		const response = await fetch(
+			'https://docs.google.com/forms/d/e/1FAIpQLSeZG0ZFC29GESiazhx4reSPvPo1OV5IVrz1AIDSfzp-myHZWg/formResponse',
+			{
+				method: 'POST',
+				body: formData,
+				mode: 'no-cors',
+			}
+		);
+		console.log('Form submitted successfully');
+		document.getElementById('name').value = '';
+		document.getElementById('phone').value = '';
+		document.getElementById('lesson').value = '';
+	} catch (error) {
+		console.error('Error submitting form:', error);
+	}
 });

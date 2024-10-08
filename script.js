c6381c10b41820593faaf749c70eb6bfd124cb8f
Kazi <uidboy01@gmail.com>
2024-10-09 01:47:53 +0500

    some optimizations

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 const form = document.getElementById('my-form');
 const errorMessage = document.getElementById('error-message');
+const submitButton = form.querySelector('.form-submit-btn');
 
 form.addEventListener('submit', async function (event) {
 	event.preventDefault();
-	const name = document.getElementById('name').value.trim();
-	const phone = document.getElementById('phone').value.trim();
-	const lesson = document.getElementById('lesson').value.trim();
 
-	if (name === '' || phone === '' || lesson === '') {
+	const nameInput = document.getElementById('name');
+	const phoneInput = document.getElementById('phone');
+	const lessonInput = document.getElementById('lesson');
+
+	const name = nameInput.value.trim();
+	const phone = phoneInput.value.trim();
+	const lesson = lessonInput.value.trim();
+
+	if (!name || !phone || !lesson) {
 		errorMessage.style.display = 'block';
 		return;
 	} else {
 		errorMessage.style.display = 'none';
 	}
 
+	submitButton.disabled = true;
+
 	const formData = new URLSearchParams();
 	formData.append('entry.1537435478', name);
 	formData.append('entry.1907448561', phone);
 	formData.append('entry.1034865502', lesson);
+
 	try {
 		const response = await fetch(
 			'https://docs.google.com/forms/d/e/1FAIpQLSeZG0ZFC29GESiazhx4reSPvPo1OV5IVrz1AIDSfzp-myHZWg/formResponse',
@@ -27,11 +36,20 @@ form.addEventListener('submit', async function (event) {
 				mode: 'no-cors',
 			}
 		);
-		console.log('Form submitted successfully');
-		document.getElementById('name').value = '';
-		document.getElementById('phone').value = '';
-		document.getElementById('lesson').value = '';
+
+		if (response.ok || response.status === 0) {
+			console.log('Form submitted successfully');
+			nameInput.value = '';
+			phoneInput.value = '';
+			lessonInput.value = '';
+		} else {
+			throw new Error('Form submission failed');
+		}
 	} catch (error) {
 		console.error('Error submitting form:', error);
+		errorMessage.textContent = 'Ошибка отправки формы, попробуйте позже.';
+		errorMessage.style.display = 'block';
+	} finally {
+		submitButton.disabled = false;
 	}
 });
